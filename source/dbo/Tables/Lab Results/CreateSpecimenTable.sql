7072870898c2fea14b03d8ca284a0a6a164b041a
Nancy Pickering <46570147+aptemus@users.noreply.github.com>
2019-11-29 09:44:27 +0000

    Create LabSpecimenTable

---
+++ source/dbo/Tables/Lab Results/CreateSpecimenTable.sql
@@ -0,0 +1,29 @@
+CREATE TABLE [dbo].[LabSpecimen]
+(
+	-- This primary key
+	[LabSpecimenId] [int] IDENTITY(1,1) NOT NULL,
+	-- LabBase primary key
+	[ReferenceLaboratoryNumber] [nvarchar](50) NOT NULL,
+	--TODO: CONSIDER RECORDS EARLIER THAN 2014 - DO THESE NOT HAVE A REFLABNO?
+	[SpecimenDate] [datetime] NULL,
+	[SpecimenTypeCode] [nvarchar](255) NULL,
+	[LaboratoryName] [nvarchar](65) NULL,
+	[ReferenceLaboratory] [nvarchar](65) NULL,
+	[Species] [varchar](50) NULL,
+	[PatientNhsNumber] [nvarchar](12) NULL,
+	[PatientBirthDate] [datetime] NULL,
+	[PatientName] [nvarchar](150) NULL,
+	[PatientSex] [nvarchar](1) NULL,
+	[PatientAddress] [nvarchar](255) NULL,
+	[PatientPostcode] [nvarchar](10) NULL,
+
+	CONSTRAINT [PK_LabSpecimenId] PRIMARY KEY CLUSTERED (
+		[LabSpecimenId] ASC
+	)
+) 
+
+ON [PRIMARY]
+GO
+
+CREATE NONCLUSTERED INDEX IX_LabSpecimen_ReferenceLaboratoryNumber ON dbo.LabSpecimen(ReferenceLaboratoryNumber)
+GO
