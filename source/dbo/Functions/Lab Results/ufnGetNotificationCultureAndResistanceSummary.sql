9578b63df63547a2c153efdd48801ce4f4257278
Nancy Pickering <46570147+aptemus@users.noreply.github.com>
2020-01-03 10:46:05 +0000

    NTBS-691: function to return a culture and resistance summary to the application

---
+++ source/dbo/Functions/Lab Results/ufnGetNotificationCultureAndResistanceSummary.sql
@@ -0,0 +1,25 @@
+﻿CREATE FUNCTION [dbo].[ufnGetNotificationCultureAndResistanceSummary]
+(
+	@NTBSId int
+)
+
+RETURNS TABLE
+AS
+
+RETURN
+SELECT	cars.NotificationId
+		,cars.CulturePositive
+		,cars.Species
+		,cars.EarliestSpecimenDate
+		,cars.DrugResistanceProfile
+		,cars.INH AS 'Isoniazid'
+		,cars.RIF AS 'Rifampicin'
+		,cars.PYR AS 'Pyrazinamide'
+		,cars.ETHAM AS 'Ethambutol'
+		,cars.AMINO AS 'Aminoglycocide'
+		,cars.QUIN AS 'Quinolone'
+		,cars.MDR
+		,cars.XDR
+      
+  FROM [dbo].[CultureAndResistanceSummary] cars
+  WHERE cars.NotificationId = @NTBSId
