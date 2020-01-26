81f094450fd539cdbaad6550adc0687d354e9848
Nancy Pickering <46570147+aptemus@users.noreply.github.com>
2020-01-26 14:59:50 +0000

    NTBS-840: Map Failed to Unknown

--- source/dbo/Stored Procedures/Lab Results/uspResultMapping.sql
+++ source/dbo/Stored Procedures/Lab Results/uspResultMapping.sql
@@ -10,8 +10,10 @@
 		INSERT INTO ResultMapping VALUES ('Resistant', 'Resistant', 1)
 		INSERT INTO ResultMapping VALUES ('S', 'Sensitive', 2)
 		INSERT INTO ResultMapping VALUES ('Sensitive', 'Sensitive', 2)
-		INSERT INTO ResultMapping VALUES ('F', 'Failed', 3)
-		INSERT INTO ResultMapping VALUES ('Failed', 'Failed', 3)
+		--Failed and Unknown are synonymous for our purposes, but are mapped to different ranks
+		--in case it is useful for debugging
+		INSERT INTO ResultMapping VALUES ('F', 'Unknown', 3)
+		INSERT INTO ResultMapping VALUES ('Failed', 'Unknown', 3)
 		INSERT INTO ResultMapping VALUES ('U', 'Unknown', 4)
 		INSERT INTO ResultMapping VALUES ('Unknown', 'Unknown', 4)
 		INSERT INTO ResultMapping VALUES ('Awaiting', 'Awaiting', 5)
