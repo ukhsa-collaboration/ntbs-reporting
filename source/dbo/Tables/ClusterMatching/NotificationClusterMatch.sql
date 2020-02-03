cdf707e9eff88838af83ccbba044b032c00700ea
Conor Sheehan <Conor.Sheehan@softwire.com>
2020-02-03 10:23:08 +0000

    NTBS-584: Implement naive cluster matching table and view for consumption by NTBS scheduled job

---
+++ source/dbo/Tables/ClusterMatching/NotificationClusterMatch.sql
@@ -0,0 +1,15 @@
+﻿/***************************************************************************************************
+Desc:    Initial naive implementation to allow work on NTBS-835 to proceed. Schema is very much
+		 subject to change.
+         
+**************************************************************************************************/
+
+CREATE TABLE [dbo].[NotificationClusterMatch](
+	[NotificationId] INT NOT NULL,
+	[ClusterId] [NVARCHAR](20) NULL
+
+	 CONSTRAINT [PK_NotificationClusterMatch] PRIMARY KEY CLUSTERED (
+		[NotificationId] ASC
+	)
+)
+GO
---
+++ source/dbo/Views/ClusterMatching/vwNotificationClusterMatch.sql
@@ -0,0 +1,36 @@
+﻿/***************************************************************************************************
+Desc:    View for purposes of representing changes that need to be made in NTBS to reflect
+		 the current state of cluster matching to NTBS records.
+         
+**************************************************************************************************/
+
+CREATE VIEW [dbo].[vwNotificationClusterMatch]
+	AS 
+	
+		/*
+			Include records from the reporting database when they are in parity
+			with NTBS cached version of the cluster matching.
+		*/
+		SELECT 
+			ncm1.NotificationId, 
+			ncm1.ClusterId
+		FROM [dbo].[NotificationClusterMatch] ncm1
+		LEFT OUTER JOIN [$(NTBS)].[dbo].[Notification] n1
+			ON n1.NotificationId = ncm1.NotificationId
+		WHERE 
+			ncm1.ClusterId <> n1.ClusterId
+
+		UNION ALL
+
+		/*
+			Also include records where NTBS contains a value, but reporting
+			does not have a value. Hypothetically a cluster was unmatched to
+			the notification.
+		*/
+		SELECT 
+			n2.NotificationId,
+			NULL AS ClusterId
+		FROM [$(NTBS)].[dbo].[Notification] n2
+		LEFT OUTER JOIN [dbo].[NotificationClusterMatch] ncm2
+			ON n2.NotificationId = ncm2.NotificationId
+		WHERE ncm2.NotificationId IS NULL
