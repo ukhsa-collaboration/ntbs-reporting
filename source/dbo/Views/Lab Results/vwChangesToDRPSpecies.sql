4bee26eda1c0939ba3612d7eeb765ce4f49747ff
Nancy Pickering <46570147+aptemus@users.noreply.github.com>
2020-02-07 09:25:02 +0000

    NTBS-886: Provide a view of notifications where the values for Species and Drug Resistance Profile cached in NTBS need to be updated

---
+++ source/dbo/Views/Lab Results/vwChangesToDRPSpecies.sql
@@ -0,0 +1,16 @@
+﻿CREATE VIEW [dbo].[vwChangesToDRPSpecies]
+	AS 
+
+	SELECT	 drp.[NotificationId]
+			,drp.[Species] as 'Current Species'
+			,drp.[DrugResistanceProfileString] as 'Current DrugResistanceProfile'
+			,rn.Species as 'New Species'
+			,rn.DrugResistanceProfile as 'New DrugResistanceProfile'
+	FROM [$(NTBS)].[dbo].[DrugResistanceProfile] drp
+			INNER JOIN [$(NTBS)].[dbo].[Notification] n ON n.NotificationId = drp.NotificationId
+			--TODO: Update this to reusable notification when NTBS-804 is implemented
+			LEFT OUTER JOIN [dbo].[CultureAndResistanceSummary] rn ON rn.NotificationId = drp.NotificationId
+	WHERE	n.NotificationStatus NOT IN ('Draft', 'Deleted')
+			AND (((drp.Species != rn.Species) OR (drp.Species IS NULL AND rn.Species IS NOT NULL))
+			OR ((drp.DrugResistanceProfileString != rn.DrugResistanceProfile) OR (drp.DrugResistanceProfileString IS NULL AND rn.DrugResistanceProfile IS NOT NULL)))
+
