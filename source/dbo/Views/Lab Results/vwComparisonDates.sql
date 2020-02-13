82fa2263f146667dc80ef354c84013bddbf8dcc3
Nancy Pickering <46570147+aptemus@users.noreply.github.com>
2020-02-13 08:43:38 +0000

    NTBS-909: view to simplify obtaining comparison dates for matching database

---
+++ source/dbo/Views/Lab Results/vwComparisonDates.sql
@@ -0,0 +1,10 @@
+﻿/*this is a view for use in the specimen matching database, retrieving dates and DRP of interest when deciding if a specimen date falls within
+the expected time period of treatment for a given notification*/
+CREATE VIEW [dbo].[vwComparisonDates]
+	AS
+
+SELECT n.NotificationId, n.NotificationDate, drp.DrugResistanceProfileString AS DrugResistanceProfile, n.NotificationDate as 'OutcomeDate'
+    FROM [$(NTBS)].[dbo].[Notification] n
+	INNER JOIN [$(NTBS)].[dbo].DrugResistanceProfile drp ON drp.NotificationId = n.NotificationId
+	WHERE n.NotificationId IN (SELECT rclientsourceID from [$(NTBS_Specimen_Matching)].[dbo].PotentialMatchComparisonDates)
+
