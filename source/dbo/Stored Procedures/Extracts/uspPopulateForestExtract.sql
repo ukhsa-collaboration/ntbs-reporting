370f9bb8c04af5c8a004dfaaa7c36a9fb1f3075c
ZOO\FIF <farman.farmanov@softwire.com>
2020-05-03 23:22:51 +0100

    NTBS-985: Add ets records into Forest Extract

--- source/dbo/Stored Procedures/Extracts/uspPopulateForestExtract.sql
+++ source/dbo/Stored Procedures/Extracts/uspPopulateForestExtract.sql
@@ -33,7 +33,6 @@ BEGIN
 			JOIN [$(NTBS)].[ReferenceData].[Site] sites ON notificationSite.SiteId = sites.SiteId 
 			GROUP BY NotificationId
 
-	
 		INSERT INTO ForestExtract (
 			-- NotificationId,
 			CaseId
@@ -59,76 +58,75 @@ BEGIN
 			)
 		SELECT
 			--reusableNotification.NotificationId,
-			CAST(NtbsId AS bigint) AS CaseId,
-			CONVERT(varchar(10), NotificationDate, 103) as CaseReportDate,
-			Forename,
-			Surname,
-			reusableNotification.NhsNumber,
-			DateOfBirth,
-			pt.Address AS AddressLine,
-			reusableNotification.NoFixedAbode AS NoFixedAbode,
-			reusableNotification.Postcode,
+			CAST(NtbsId AS bigint)												AS CaseId,
+			CONVERT(varchar(10), NotificationDate, 103)							AS CaseReportDate,
+			Forename															AS Forename,
+			Surname																AS Surname,
+			reusableNotification.NhsNumber										As NhsNumber,
+			DateOfBirth															As DateOfBirth,
+			patient.Address														AS AddressLine,
+			reusableNotification.NoFixedAbode									AS NoFixedAbode,
+			reusableNotification.Postcode										AS Postcode,
 			CASE Sex 
 				WHEN 'Female' THEN 'F' 
 				WHEN 'Male' THEN 'M' 
 				WHEN 'Unknown' THEN 'U' 
-			END AS Sex,
-			Hospital,
-			LocalAuthority AS ResidenceLocalAuthority,
+			END																	AS Sex,
+			Hospital															AS Hospital,
+			LocalAuthority														AS ResidenceLocalAuthority,
 			-- rn.PCT as [Hpu],
-			phec.Name AS Region,
-			occupation.[Role] AS Occupation,
-			occupation.Sector AS OccupationCategory,
-			EthnicGroup,
-			reusableNotification.UkBorn AS UKBorn,
-			BirthCountry,
-			UkEntryYear,
-			CONVERT(varchar, SymptomOnsetDate, 103) AS SymptomOnset,
-			CONVERT(varchar, PresentedDate, 103) AS DatePresented,
-			CONVERT(varchar, DiagnosisDate, 103) AS DateOfDiagnosis,
-			CONVERT(varchar, StartOfTreatmentDate, 103) AS StartOfTreatment,
-
-			reusableNotification.DrugMisuse AS DrugUse,
-			dbo.GetSiteDiseaseDuration(DrugMisuseInLast5Years) AS DrugMisuseLast5Years,
-			dbo.GetSiteDiseaseDuration(DrugMisuseMoreThan5YearsAgo) AS DrugUseMoreThan5YearsAgo,
-			dbo.GetSiteDiseaseDuration(CurrentDrugMisuse) AS CurrentDrugUse,
-	
-			reusableNotification.Homeless,
-			dbo.GetSiteDiseaseDuration(HomelessInLast5Years) AS HomelessLast5Years,
-			dbo.GetSiteDiseaseDuration(HomelessMoreThan5YearsAgo) AS HomelessMoreThan5YearsAgo,
-			dbo.GetSiteDiseaseDuration(CurrentlyHomeless) AS CurrentlyHomeless,
-
-			reusableNotification.Prison,
-			dbo.GetSiteDiseaseDuration(InPrisonInLast5Years) AS InPrisonInLast5Years,
-			dbo.GetSiteDiseaseDuration(InPrisonMoreThan5YearsAgo) AS PrisonMoreThan5YearsAgo,
+			phec.Name															AS Region,
+			occupation.[Role]													AS Occupation,
+			occupation.Sector													AS OccupationCategory,
+			EthnicGroup															AS EthnicGroup,
+			reusableNotification.UkBorn											AS UKBorn,
+			BirthCountry														AS BirthCountry,
+			UkEntryYear															AS UkEntryYear,
+			CONVERT(varchar, SymptomOnsetDate, 103)								AS SymptomOnset,
+			CONVERT(varchar, PresentedDate, 103)								AS DatePresented,
+			CONVERT(varchar, DiagnosisDate, 103)								AS DateOfDiagnosis,
+			CONVERT(varchar, StartOfTreatmentDate, 103)							AS StartOfTreatment,
+			reusableNotification.DrugMisuse										AS DrugUse,
+			dbo.GetSiteDiseaseDuration(DrugMisuseInLast5Years)					AS DrugMisuseLast5Years,
+			dbo.GetSiteDiseaseDuration(DrugMisuseMoreThan5YearsAgo)				AS DrugUseMoreThan5YearsAgo,
+			dbo.GetSiteDiseaseDuration(CurrentDrugMisuse)						AS CurrentDrugUse,
+			reusableNotification.Homeless										AS Homeless,
+			dbo.GetSiteDiseaseDuration(HomelessInLast5Years)					AS HomelessLast5Years,
+			dbo.GetSiteDiseaseDuration(HomelessMoreThan5YearsAgo)				AS HomelessMoreThan5YearsAgo,
+			dbo.GetSiteDiseaseDuration(CurrentlyHomeless)						AS CurrentlyHomeless,
+			reusableNotification.Prison											AS Prison,
+			dbo.GetSiteDiseaseDuration(InPrisonInLast5Years)					AS InPrisonInLast5Years,
+			dbo.GetSiteDiseaseDuration(InPrisonMoreThan5YearsAgo)				AS PrisonMoreThan5YearsAgo,
 			dbo.GetSiteDiseaseDuration(CurrentlyInPrisonOrInPrisonWhenFirstSeen) AS CurrentlyInprisonOrWhenFirstSeen,
-
-			reusableNotification.AlcoholMisuse AS AlcoholUse,
-
-			lab.[SpecimenTypeCode] AS SmearSample,
-			'Positive' AS SmearSampleResult,
-			lab.SpecimenDate AS SpecimenDate,
-			lab.ReferenceLaboratoryNumber,
-
-			GETUTCDATE() As ExtractDate,
-			pt.LocalPatientId,
-			Age,
-			--hospital.CaseManagerUsername AS OwnerUserId,
-			CaseManager,
-			reusableNotification.Consultant AS PatientsConsultant,
-			 --ClusterNumber
-			diseaseSites.Description AS DiseaseSites
-
+			reusableNotification.AlcoholMisuse									AS AlcoholUse,
+			lab.[SpecimenTypeCode]												AS SmearSample,
+			'Positive'															AS SmearSampleResult,
+			lab.SpecimenDate													AS SpecimenDate,
+			lab.ReferenceLaboratoryNumber										AS ReferenceLaboratoryNumber,
+			GETUTCDATE()														AS ExtractDate,
+			patient.LocalPatientId												AS LocalPatientId,
+			Age																	AS Age,
+			--hospital.CaseManagerUsername										AS OwnerUserId,
+			CaseManager															AS CaseManager,
+			reusableNotification.Consultant										AS PatientsConsultant,
+			 --ClusterNumber													AS ClusterNumber,
+			diseaseSites.Description											AS DiseaseSites
 		FROM ([dbo].[ReusableNotification] reusableNotification
-		INNER JOIN [$(NTBS_Specimen_Matching)].dbo.NotificationSpecimenMatch specimentMatch ON specimentMatch.NotificationID = reusableNotification.NtbsId
-		INNER JOIN [dbo].LabSpecimen lab ON lab.ReferenceLaboratoryNumber = specimentMatch.ReferenceLaboratoryNumber)
-		LEFT JOIN [$(NTBS)].[dbo].Patients pt ON pt.NotificationId = reusableNotification.NtbsId
-		LEFT JOIN [$(NTBS)].[dbo].HospitalDetails hospital ON hospital.NotificationId = pt.NotificationId 
-		LEFT JOIN [$(NTBS)].[ReferenceData].Occupation occupation ON occupation.OccupationId = pt.OccupationId
+		INNER JOIN [$(NTBS_Specimen_Matching)].dbo.NotificationSpecimenMatch specimenMatch ON specimenMatch.NotificationID = reusableNotification.NtbsId
+		INNER JOIN [dbo].LabSpecimen lab ON lab.ReferenceLaboratoryNumber = specimenMatch.ReferenceLaboratoryNumber)
+		LEFT JOIN [$(NTBS)].[dbo].Patients patient ON patient.NotificationId = reusableNotification.NtbsId
+		LEFT JOIN [$(NTBS)].[dbo].HospitalDetails hospital ON hospital.NotificationId = patient.NotificationId 
+		LEFT JOIN [$(NTBS)].[ReferenceData].Occupation occupation ON occupation.OccupationId = patient.OccupationId
 		LEFT JOIN [$(NTBS)].[ReferenceData].PHEC phec ON phec.Code = TreatmentPhecCode
 		LEFT JOIN [$(NTBS)].[ReferenceData].PostcodeLookup postcode ON postcode.Postcode = REPLACE(reusableNotification.Postcode, ' ', '')
-		LEFT JOIN @TempDiseaseSites diseaseSites ON diseaseSites.NotificationId = pt.NotificationId
+		LEFT JOIN @TempDiseaseSites diseaseSites ON diseaseSites.NotificationId = patient.NotificationId
 		WHERE TreatmentPhecCode NOT IN ('PHECNI', 'PHECSCOT', 'PHECWAL') OR phec.Code IS NULL -- Exclude Wales, Scotland and Northern Ireland
+
+		INSERT INTO [dbo].ForestExtract 
+		SELECT etsForest.*
+		FROM [$(ETS)].dbo.ETSOxfordExtract etsForest
+		LEFT JOIN [dbo].[ReusableNotification] reusableNotification ON reusableNotification.EtsId = etsForest.CaseId
+		WHERE reusableNotification.NtbsId IS NULL
 	END TRY
 	BEGIN CATCH
 		EXEC dbo.uspHandleException
