a34a0d582ac4f941e39faf6d799bf5cfb109ecb1
Jan Mikolajczak <jan.mikolajczak@softwire.com>
2020-05-28 13:52:58 +0100

    NTBS-1347 Sort out the order of treatment events on the same day

--- source/dbo/Functions/Reusable Notification/ufnGetPeriodicOutcome.sql
+++ source/dbo/Functions/Reusable Notification/ufnGetPeriodicOutcome.sql
@@ -17,13 +17,21 @@ RETURN
 --DECLARE @TimePeriod AS INT = 1
 
 
+-- The logic of this code must stay in sync between the application and the reporting service.
+-- The source of truth is documented in https://airelogic-nis.atlassian.net/wiki/spaces/R2/pages/599687169/Outcomes+logic
 SELECT TOP(1) 
 	@NotificationId AS 'NotificationId',
 	@TimePeriod AS 'Period', 
 	--ensure that if there is an ending and not-ending event (like a transfer or restart) on the last day in the period that has event data
 	--(e.g. outcome and restart recorded on the same day)
-	--the restart will be taken in preference to the outcome
-	(CASE WHEN te.TreatmentEventType = 'TreatmentOutcome' THEN 0 ELSE 1 END) AS EventOrder,
+	-- that we will have a consistent order, with restart taken in preference to the outcome
+	(CASE
+		WHEN te.TreatmentEventType = 'TreatmentStart' THEN 1
+		WHEN te.TreatmentEventType = 'TransferOut' THEN 2
+		WHEN te.TreatmentEventType = 'TransferIn' THEN 3
+		WHEN te.TreatmentEventType = 'TreatmentRestart' THEN 4
+		WHEN te.TreatmentEventType = 'TreatmentOutcome' THEN 5
+		END) AS EventOrder,
 	--calculate whether the event is an ending one or not
 	(CASE 
 		WHEN tro.TreatmentOutcomeType != 'NotEvaluated' THEN 1 
