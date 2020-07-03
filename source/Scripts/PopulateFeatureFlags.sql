d0fa7162eb986be4e7e8026282ab8358bb118cd1
Nancy Pickering <46570147+aptemus@users.noreply.github.com>
2020-07-03 10:57:37 +0100

    NTBS-1500: implement flag to ensure records from a test instance of NTBS are not accidentally copied into live

---
+++ source/Scripts/PopulateFeatureFlags.sql
@@ -0,0 +1,9 @@
+﻿--this can be used to set up the ReportingFeatureFlags table in your chosen environment - set the values 
+--according to what you need.  It assumes the table is currently empty
+
+IF (SELECT COUNT(1) FROM [dbo].[ReportingFeatureFlags]) = 0
+BEGIN
+	INSERT INTO [dbo].[ReportingFeatureFlags](IncludeNTBS, SourceUsersFromNTBS, Comment)
+	VALUES(0, 0, 'The first flag ensures records are not sourced from NTBS if it is not active. The second flag allows users to be sourced via
+	the NTBS user sync process once that is active, even if the application itself is not yet in use')
+END
---
+++ source/dbo/Tables/Reusable Notification/ReportingFeatureFlags.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[ReportingFeatureFlags]
+(
+    --this table has some flags which allow us to control how the reporting service interacts with NTBS
+    --there is no population script, as the values are environment-specific
+	[Id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY, 
+    [IncludeNTBS] BIT NULL, 
+    [SourceUsersFromNTBS] BIT NULL, 
+    [Comment] NVARCHAR(MAX) NULL
+)
