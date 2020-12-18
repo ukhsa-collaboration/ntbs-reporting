d5e504abb9133911ddae208f1eade47ae8d3a302
Nancy Pickering <46570147+aptemus@users.noreply.github.com>
2020-12-18 18:52:46 +0000

    NTBS-1660 - changed denominator for calculating the percentage of resistant cases in the region or service

--- source/dbo/Views/KPI/vwPhecKPI.sql
+++ source/dbo/Views/KPI/vwPhecKPI.sql
@@ -5,9 +5,8 @@ AS
 		COALESCE (Q1.NumberofNotifications, 0) As NumberOfNotifications,
 		COALESCE (Q1.HIVDenominator, 0) As HIVDenominator,
 		COALESCE (CAST((Q1.CPCount * 100.0) / NULLIF(Q1.NumberOfNotifications,0) AS DECIMAL(10, 1)), 0.0) AS '%Positive', 
-		COALESCE (CAST((Q1.ResistantCount * 100.0) / NULLIF(Q1.NumberOfNotifications,0) AS DECIMAL(10, 1)), 0.0) As '%Resistant',
-		--this should be on Q1.HIVDenominator but it causes a divide by zero error?  Maybe use a NULLIF?
-		COALESCE (CAST((Q1.HIVOffered * 100.0) / NULLIF(Q1.hivdenominator,0) AS DECIMAL(10, 1)), 0.0) As '%HIVOffered',
+		COALESCE (CAST((Q1.ResistantCount * 100.0) / NULLIF(Q1.CPCount,0) AS DECIMAL(10, 1)), 0.0) As '%Resistant',
+		COALESCE (CAST((Q1.HIVOffered * 100.0) / NULLIF(Q1.HIVDenominator,0) AS DECIMAL(10, 1)), 0.0) As '%HIVOffered',
 		COALESCE (CAST((Q1.TreatmentDelays * 100.0) / NULLIF(Q1.NumberOfPulmonaryNotifications, 0) AS DECIMAL(10, 1)), 0.0) As '%TreatmentDelay'
 		
 		FROM [$(NTBS_R1_Geography_Staging)].[dbo].[PHEC] p
