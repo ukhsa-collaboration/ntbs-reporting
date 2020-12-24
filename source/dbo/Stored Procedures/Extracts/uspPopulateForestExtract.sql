39d784f6f07b97dcd38e306934837b32f1044959
Nancy Pickering <46570147+aptemus@users.noreply.github.com>
2020-12-24 10:06:58 +0000

    NTBS-1465: updates to FOREST extract

--- source/dbo/Stored Procedures/Extracts/uspPopulateForestExtract.sql
+++ source/dbo/Stored Procedures/Extracts/uspPopulateForestExtract.sql
@@ -12,11 +12,11 @@ BEGIN
 		DECLARE @TempDiseaseSites TABLE
 		(
 		   NotificationId int,
-		   description nvarchar(2000)
+		   [Description] nvarchar(2000)
 		);
 
 		INSERT INTO @TempDiseaseSites
-		SELECT NotificationId, Description = STRING_AGG(Description, N', ')
+		SELECT NotificationId, [Description] = STRING_AGG([Description], N',')
 			FROM [$(NTBS)].[dbo].[NotificationSite] notificationSite
 			JOIN [$(NTBS)].[ReferenceData].[Site] sites ON notificationSite.SiteId = sites.SiteId 
 			GROUP BY NotificationId
@@ -24,25 +24,47 @@ BEGIN
 		INSERT INTO ForestExtract (
 			-- NotificationId,
 			CaseId
-			, Casereportdate, Forename, Surname, NHSNumber, DateOfBirth, AddressLine1
-			, NoFixedAbode, Postcode, Sex, Hospital, ResidenceLocalAuthority
-			--, ResidenceHPU
-			, HPAResidenceRegion
-			, Occupation, OccupationCategory, EthnicGroup
-			, UKBorn, BirthCountry, UKEntryYear
-			, SymptomOnset, DatePresented, DateOfDiagnosis, StartOfTreatment
-			, DrugUse 
-			, DrugUseLast5Years, DrugUseMoreThan5YearsAgo, CurrentDrugUse
-			, Homeless 
-			, HomelessLast5Years, HomelessMoreThan5YearsAgo, CurrentlyHomeless
-			, Prison 
-			, PrisonLast5Years, PrisonMoreThan5YearsAgo, CurrentlyInPrisonOrWhenFirstSeen
-			, AlcoholUse
-			, SmearSample, SmearSampleResult, SpecimenDate, ReferenceLaboratoryNumber
-			, ExtractDate
-			, Localpatientid, Age
-			--, OwnerUserId
-			, CaseManager, PatientsConsultant, DiseaseSites
+			,Casereportdate
+			,Forename
+			,Surname
+			,NHSNumber
+			,DateOfBirth
+			,AddressLine1
+			,NoFixedAbode
+			,Postcode
+			,Sex
+			,Hospital
+			,ResidenceLocalAuthority
+			,ResidenceHPU
+			,HPAResidenceRegion
+			,Occupation
+			,OccupationCategory
+			,EthnicGroup
+			,UKBorn
+			,BirthCountry
+			,UKEntryYear
+			,SymptomOnset
+			,DatePresented
+			,DateOfDiagnosis
+			,StartOfTreatment
+			,DrugUse 
+			,DrugUseLast5Years, DrugUseMoreThan5YearsAgo, CurrentDrugUse
+			,Homeless 
+			,HomelessLast5Years, HomelessMoreThan5YearsAgo, CurrentlyHomeless
+			,Prison 
+			,PrisonLast5Years, PrisonMoreThan5YearsAgo, CurrentlyInPrisonOrWhenFirstSeen
+			,AlcoholUse
+			,SmearSample
+			,SmearSampleResult
+			,SpecimenDate
+			,ReferenceLaboratoryNumber
+			,ExtractDate
+			,Localpatientid
+			,Age
+			,OwnerUserId
+			,CaseManager 
+			,PatientsConsultant
+			,DiseaseSites
 			)
 		SELECT
 			--reusableNotification.NotificationId,
@@ -52,7 +74,7 @@ BEGIN
 			Surname																							AS Surname,
 			reusableNotification.NhsNumber																	AS NhsNumber,
 			DateOfBirth																						AS DateOfBirth,
-			patient.Address																					AS AddressLine,
+			patient.[Address]																				AS AddressLine,
 			reusableNotification.NoFixedAbode																AS NoFixedAbode,
 			reusableNotification.Postcode																	AS Postcode,
 			CASE Sex 
@@ -62,13 +84,13 @@ BEGIN
 			END																								AS Sex,
 			Hospital																						AS Hospital,
 			LocalAuthority																					AS ResidenceLocalAuthority,
-			-- rn.PCT as [Hpu],
-			phec.Name																						AS Region,
+			nacs.HPU																						AS ResidenceHPU,
+			rn.ResidencePhec																				AS Region,
 			occupation.[Role]																				AS Occupation,
 			occupation.Sector																				AS OccupationCategory,
 			EthnicGroup																						AS EthnicGroup,
 			reusableNotification.UkBorn																		AS UKBorn,
-			BirthCountry																					AS BirthCountry,
+			UPPER(BirthCountry)																				AS BirthCountry,
 			UkEntryYear																						AS UkEntryYear,
 			CONVERT(varchar, SymptomOnsetDate, 103)															AS SymptomOnset,
 			CONVERT(varchar, PresentedDate, 103)															AS DatePresented,
@@ -94,22 +116,41 @@ BEGIN
 			GETUTCDATE()																					AS ExtractDate,
 			patient.LocalPatientId																			AS LocalPatientId,
 			Age																								AS Age,
-			--hospital.CaseManagerUsername																	AS OwnerUserId,
+			hospital.CaseManagerUsername																	AS OwnerUserId,
 			CaseManager																						AS CaseManager,
 			reusableNotification.Consultant																	AS PatientsConsultant,
 			 --ClusterNumber																				AS ClusterNumber,
-			diseaseSites.Description																		AS DiseaseSites
-		FROM ([dbo].[ReusableNotification] reusableNotification
-		INNER JOIN [$(NTBS_Specimen_Matching)].dbo.NotificationSpecimenMatch specimenMatch ON specimenMatch.NotificationID = reusableNotification.NtbsId
-		INNER JOIN [dbo].LabSpecimen lab ON lab.ReferenceLaboratoryNumber = specimenMatch.ReferenceLaboratoryNumber)
-		LEFT JOIN [$(NTBS)].[dbo].Patients patient ON patient.NotificationId = reusableNotification.NtbsId
+			diseaseSites.[Description]																		AS DiseaseSites
+		FROM ([dbo].[ReusableNotification] rn
+		INNER JOIN [$(NTBS_Specimen_Matching)].dbo.NotificationSpecimenMatch nsm ON nsm.NotificationID = rn.NtbsId
+		INNER JOIN [dbo].LabSpecimen lab ON lab.ReferenceLaboratoryNumber = nsm.ReferenceLaboratoryNumber)
+		LEFT JOIN [$(NTBS)].[dbo].Patients patient ON patient.NotificationId = rn.NtbsId
 		LEFT JOIN [$(NTBS)].[dbo].HospitalDetails hospital ON hospital.NotificationId = patient.NotificationId 
 		LEFT JOIN [$(NTBS)].[ReferenceData].Occupation occupation ON occupation.OccupationId = patient.OccupationId
-		LEFT JOIN [$(NTBS)].[ReferenceData].PHEC phec ON phec.Code = TreatmentPhecCode
-		LEFT JOIN [$(NTBS)].[ReferenceData].PostcodeLookup postcode ON postcode.Postcode = REPLACE(reusableNotification.Postcode, ' ', '')
+		LEFT JOIN [$(NTBS_R1_Geography_Staging)].[dbo].[Reduced_Postcode_file] pl ON pl.Pcode = REPLACE(rn.Postcode, ' ', '')
+		LEFT JOIN [$(ETS)].[dbo].[NACS_pctlookup] nacs ON nacs.PCT_code = pl.PctCode
 		LEFT JOIN @TempDiseaseSites diseaseSites ON diseaseSites.NotificationId = patient.NotificationId
-		WHERE specimenMatch.MatchType = 'Confirmed' 
-			AND (TreatmentPhecCode NOT IN ('PHECNI', 'PHECSCOT', 'PHECWAL') OR phec.Code IS NULL) -- Exclude Wales, Scotland and Northern Ireland
+		WHERE nsm.MatchType = 'Confirmed' 
+			AND (rn.residencePhecCode NOT IN ('PHECNI', 'PHECSCOT', 'PHECWAL') OR rn.ResidencePhecCode IS NULL); -- Exclude Wales, Scotland and Northern Ireland
+
+		WITH VNTR AS
+		(SELECT DISTINCT
+			nsm.ReferenceLaboratoryNumber, v.VntrProfile
+		FROM [$(NTBS_Specimen_Matching)].[dbo].[NotificationSpecimenMatch] nsm
+			INNER JOIN [$(Labbase2)].[dbo].[Anonymised] a ON a.ReferenceLaboratoryNumber = nsm.ReferenceLaboratoryNumber
+			INNER JOIN [$(Labbase2)].[dbo].[VntrProfile] v ON v.LabDataId = a.LabDataID
+		WHERE nsm.MatchType = 'Confirmed')
+
+		--set VNTR
+		UPDATE dbo.ForestExtract
+			SET VntrProfile = v.VntrProfile
+			FROM dbo.ForestExtract e
+				INNER JOIN VNTR v ON v.ReferenceLaboratoryNumber = e.ReferenceLaboratoryNumber
+
+		--convert Yorkshire and Humber to Yorkshire and The Humber
+		UPDATE dbo.ForestExtract
+			SET HPAResidenceRegion = 'Yorkshire and The Humber'
+			WHERE HPAResidenceRegion = 'Yorkshire and Humber'
 
 		INSERT INTO [dbo].ForestExtract 
 		SELECT etsForest.*
