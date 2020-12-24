dd0063f5a6312865a6c4fc48cc3e207e52bb4fe7
Nancy Pickering <46570147+aptemus@users.noreply.github.com>
2020-12-24 12:42:37 +0000

    NTBS-1543: supporting views

---
+++ source/dbo/Views/Lab Results/vwEtsDuplicateMatches.sql
@@ -0,0 +1,29 @@
+﻿/*
+	This view finds the specimens which have been matched to more than ETS notification. This typically happens when:
+	- a person has TB twice and so is notified twice, some years apart
+	- a single infection of TB is notified by two people, creating a duplicate notification record
+*/
+
+CREATE VIEW [dbo].[vwEtsDuplicateMatches]
+	AS 
+  WITH EtsNotificationSpecimenMatch AS
+  (SELECT DISTINCT n.LegacyId, a.ReferenceLaboratoryNumber, n.NotificationDate
+   FROM [$(ETS)].[dbo].[LaboratoryResult] lr
+	INNER JOIN [$(Labbase2)].[dbo].[Anonymised] a ON a.OpieId = lr.OpieId
+	INNER JOIN [$(ETS)].[dbo].[Notification] n ON n.Id = lr.NotificationId
+   WHERE lr.OpieId IS NOT NULL
+	AND lr.AuditDelete IS NULL
+	AND n.NotificationDate > '2010-01-01'
+	AND n.Submitted = 1
+	AND n.DenotificationId IS NULL),
+
+  DuplicateNotifications AS
+  (SELECT e.ReferenceLaboratoryNumber, COUNT(e.LegacyId) AS CountOfNotifs
+	FROM EtsNotificationSpecimenMatch e
+	GROUP BY e.ReferenceLaboratoryNumber
+	HAVING COUNT (e.LegacyId) > 1)
+
+  SELECT e.LegacyId, e.NotificationDate, e.ReferenceLaboratoryNumber
+  FROM
+  EtsNotificationSpecimenMatch e
+	INNER JOIN DuplicateNotifications d ON d.ReferenceLaboratoryNumber = e.ReferenceLaboratoryNumber
---
+++ source/dbo/Views/Lab Results/vwEtsSpecimenMatch.sql
@@ -0,0 +1,14 @@
+﻿CREATE VIEW [dbo].[vwEtsSpecimenMatch]
+	AS   
+	
+   SELECT DISTINCT n.LegacyId, a.ReferenceLaboratoryNumber, MAX(lr.AuditCreate) AS MostRecentMatchDate
+   FROM [$(ETS)].[dbo].[LaboratoryResult] lr
+	INNER JOIN [$(Labbase2)].[dbo].[Anonymised] a ON a.OpieId = lr.OpieId
+	INNER JOIN [$(ETS)].[dbo].[Notification] n ON n.Id = lr.NotificationId
+
+   WHERE lr.OpieId IS NOT NULL
+	AND lr.AuditDelete IS NULL
+	AND n.NotificationDate > '2010-01-01'
+	AND n.Submitted = 1
+	AND n.DenotificationId IS NULL
+	GROUP BY n.LegacyId, a.ReferenceLaboratoryNumber
