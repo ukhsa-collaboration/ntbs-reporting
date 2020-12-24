5dcc168221d769f1b802bca38735786577f7548e
Nancy Pickering <nancy@aptemus.com>
2020-12-24 14:49:08 +0000

    NTBS-1543: update to view

--- source/dbo/Views/Lab Results/vwEtsSpecimenMatch.sql
+++ source/dbo/Views/Lab Results/vwEtsSpecimenMatch.sql
@@ -1,7 +1,7 @@
 ﻿CREATE VIEW [dbo].[vwEtsSpecimenMatch]
 	AS   
 	
-   SELECT DISTINCT n.LegacyId, a.ReferenceLaboratoryNumber, MAX(lr.AuditCreate) AS MostRecentMatchDate
+   SELECT DISTINCT n.LegacyId, n.NotificationDate, a.ReferenceLaboratoryNumber, MAX(lr.AuditCreate) AS MostRecentMatchDate
    FROM [$(ETS)].[dbo].[LaboratoryResult] lr
 	INNER JOIN [$(Labbase2)].[dbo].[Anonymised] a ON a.OpieId = lr.OpieId
 	INNER JOIN [$(ETS)].[dbo].[Notification] n ON n.Id = lr.NotificationId
@@ -11,4 +11,4 @@
 	AND n.NotificationDate > '2010-01-01'
 	AND n.Submitted = 1
 	AND n.DenotificationId IS NULL
-	GROUP BY n.LegacyId, a.ReferenceLaboratoryNumber
+	GROUP BY n.LegacyId, n.NotificationDate, a.ReferenceLaboratoryNumber
