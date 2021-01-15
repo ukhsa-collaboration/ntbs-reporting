a390f046265e6d17f788296f08589428ecc0473d
Nancy Pickering <46570147+aptemus@users.noreply.github.com>
2021-01-15 18:10:51 +0000

    NTBS-1979: views for row-level security

---
+++ source/dbo/Views/Authorization/vwUser.sql
@@ -0,0 +1,5 @@
+﻿CREATE VIEW [dbo].[vwUser]
+	AS 
+	--TODO this should be replaced with a call to the User table in the Reporting database
+	SELECT Username as upn, u.DisplayName 
+	FROM [$(NTBS)].[dbo].[User] u
---
+++ source/dbo/Views/Authorization/vwUserPermissions.sql
@@ -0,0 +1,15 @@
+﻿CREATE VIEW [dbo].[vwUserPermissions]
+	AS 
+	--create one row for each AD group the user is a member of
+	--TODO: replace this with a call to the reporting user table
+	SELECT Username AS upn, COALESCE(t.Code, p.Code, A.[value]) AS 'Code', COALESCE(t.[Name], p.[Name]) AS 'Name' 
+	FROM [$(NTBS)].[dbo].[User]
+
+		CROSS APPLY STRING_SPLIT(AdGroups, ',') A
+		LEFT OUTER JOIN [$(NTBS)].[ReferenceData].[TbService] t ON t.ServiceAdGroup = A.[value]
+		LEFT OUTER JOIN [$(NTBS)].[ReferenceData].[PHEC] p ON p.AdGroup = A.[value]
+
+		WHERE 
+		A.[value] != 'Global.NIS.NTBS.Admin'
+		AND A.[value] != 'Global.NIS.NTBS.NTS'
+		
