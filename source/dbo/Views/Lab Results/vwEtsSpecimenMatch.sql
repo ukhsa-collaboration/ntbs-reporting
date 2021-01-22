9191a578d0da0741942abea69e76148a12f282ba
Nancy Pickering <nancy@aptemus.com>
2021-01-22 10:08:00 +0000

    NTBS-1543: update specimen view to include extra data

--- source/dbo/Views/Lab Results/vwEtsSpecimenMatch.sql
+++ source/dbo/Views/Lab Results/vwEtsSpecimenMatch.sql
@@ -1,14 +1,20 @@
 ﻿CREATE VIEW [dbo].[vwEtsSpecimenMatch]
 	AS   
 	
-   SELECT DISTINCT n.LegacyId, n.NotificationDate, a.ReferenceLaboratoryNumber, MAX(lr.AuditCreate) AS MostRecentMatchDate
-   FROM [$(ETS)].[dbo].[LaboratoryResult] lr
-	INNER JOIN [$(Labbase2)].[dbo].[Anonymised] a ON a.OpieId = lr.OpieId
-	INNER JOIN [$(ETS)].[dbo].[Notification] n ON n.Id = lr.NotificationId
-
-   WHERE lr.OpieId IS NOT NULL
-	AND lr.AuditDelete IS NULL
-	AND n.NotificationDate > '2010-01-01'
-	AND n.Submitted = 1
-	AND n.DenotificationId IS NULL
-	GROUP BY n.LegacyId, n.NotificationDate, a.ReferenceLaboratoryNumber
+	SELECT DISTINCT 
+		n.LegacyId 
+		,s.ReferenceLaboratoryNumber 
+		,MIN(lr.auditcreate) OVER (PARTITION BY n.LegacyID) AS EarliestMatchDate
+		,MIN(s.SpecimenDate) OVER (PARTITION BY s.ReferenceLaboratoryNumber) AS SpecimenDate 
+		,CASE WHEN n.DenotificationId IS NOT NULL THEN 1 ELSE 0 END AS Denotified
+		,CASE WHEN n.Submitted = 0 THEN 1 ELSE 0 END AS Draft
+		,MAX(CASE WHEN lr.AutoMatched = 1 THEN 1 ELSE 0 END) OVER (PARTITION BY s.ReferenceLaboratoryNumber) AS Automatched
+	FROM [$(ETS)].[dbo].[Notification] n
+	INNER JOIN [$(ETS)].[dbo].[LaboratoryResult] lr ON lr.NotificationId = n.Id
+	INNER JOIN [StandardisedLabbaseSpecimen] s ON s.OpieId = lr.OpieId
+	WHERE 
+	   n.[AuditDelete] IS NULL
+	   --include denotified and draft records in this query, so these can be highlighted as bad matches, so unlike most similar queries we do not include
+	    --n.DenotificationId IS NULL AND n.Submitted = 1
+		AND lr.OpieId IS NOT NULL
+		AND lr.AuditDelete IS NULL
