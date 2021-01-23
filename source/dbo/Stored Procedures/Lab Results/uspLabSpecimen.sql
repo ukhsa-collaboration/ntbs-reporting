c0a9c8eb8cc943458deb868364760e175083642a
Nancy Pickering <nancy@aptemus.com>
2021-01-23 10:51:01 +0000

    NTBS-1543: convert view to table and update uspLabSpecimen to re-populate it

--- source/dbo/Stored Procedures/Lab Results/uspLabSpecimen.sql
+++ source/dbo/Stored Procedures/Lab Results/uspLabSpecimen.sql
@@ -13,6 +13,7 @@ CREATE PROCEDURE [dbo].[uspLabSpecimen] AS
 
 		-- Reset
 		TRUNCATE TABLE [dbo].[LabSpecimen]
+		TRUNCATE TABLE [dbo].[EtsSpecimenMatch]
 		TRUNCATE TABLE [dbo].[StandardisedLabbaseSpecimen]
 		TRUNCATE TABLE [dbo].[StandardisedLabbaseSusceptibilityResult]
 
@@ -47,6 +48,25 @@ CREATE PROCEDURE [dbo].[uspLabSpecimen] AS
 					AND a.IsAtypicalOrganismRecord = 0
 					AND a.MergedRecord = 0
 
+		--refresh the EtsSpecimenMatch table
+		INSERT INTO [dbo].[EtsSpecimenMatch](LegacyId, ReferenceLaboratoryNumber, EarliestMatchDate, SpecimenDate, Denotified, Draft, Automatched)
+			SELECT DISTINCT 
+				n.LegacyId 
+				,s.ReferenceLaboratoryNumber 
+				,MIN(lr.auditcreate) OVER (PARTITION BY n.LegacyID) AS EarliestMatchDate
+				,MIN(s.SpecimenDate) OVER (PARTITION BY s.ReferenceLaboratoryNumber) AS SpecimenDate 
+				,CASE WHEN n.DenotificationId IS NOT NULL THEN 1 ELSE 0 END AS Denotified
+				,CASE WHEN n.Submitted = 0 THEN 1 ELSE 0 END AS Draft
+				,MAX(CASE WHEN lr.AutoMatched = 1 THEN 1 ELSE 0 END) OVER (PARTITION BY s.ReferenceLaboratoryNumber) AS Automatched
+			FROM [$(ETS)].[dbo].[Notification] n
+				INNER JOIN [$(ETS)].[dbo].[LaboratoryResult] lr ON lr.NotificationId = n.Id
+				INNER JOIN [dbo].[StandardisedLabbaseSpecimen] s ON s.OpieId = lr.OpieId
+			WHERE 
+				n.[AuditDelete] IS NULL
+				--include denotified and draft records in this query, so these can be highlighted as bad matches, so unlike most similar queries we do not include
+				--n.DenotificationId IS NULL AND n.Submitted = 1
+				AND lr.OpieId IS NOT NULL
+				AND lr.AuditDelete IS NULL
 
 		--Find the specimens to insert into LabSpecimen
 		--First fetch all the matched specimens, regardless of specimen date
---
+++ source/dbo/Tables/Lab Results/EtsSpecimenMatch.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[EtsSpecimenMatch](
+	[LegacyId] [bigint] NOT NULL,
+	[ReferenceLaboratoryNumber] [nvarchar](50) NOT NULL,
+	[EarliestMatchDate] [datetime] NULL,
+	[SpecimenDate] [datetime] NULL,
+	[Denotified] [int] NULL,
+	[Draft] [int] NULL,
+	[Automatched] [int] NULL
+) ON [PRIMARY]
+GO
