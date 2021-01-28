a8788954a6813e6da6edf1f7d33c5f19d1c7f28a
Nancy Pickering <nancy@aptemus.com>
2021-01-28 10:50:30 +0000

    NTBS-1543: added stored proc to call from the report. Also added a script for manual investgation of problem matches.

---
+++ source/Scripts/ReviewProblemSpecimenMatch.sql
@@ -0,0 +1,71 @@
+﻿
+DECLARE @ReferenceLaboratoryNumber NVARCHAR(25) -- = set parameter value here
+
+SELECT lm.*, s.ScenarioDescription
+FROM [dbo].vwComparisonOfLabMatches lm
+	LEFT OUTER JOIN [vwComparisonOfLabMatchScenarios] s ON s.Scenario = lm.Scenario
+WHERE ReferenceLaboratoryNumber = @ReferenceLaboratoryNumber
+
+SELECT 'SpecimenDetails' AS 'Info', SpecimenDate, EarliestRecordDate, LaboratoryName, PatientNhsNumber, PatientBirthDate, PatientName, 
+	PatientSex, PatientAddress, PatientPostcode 
+FROM [dbo].[LabSpecimen] 
+WHERE ReferenceLaboratoryNumber = @ReferenceLaboratoryNumber
+
+ --then all NTBS matches, including any rejected ones
+
+SELECT 'NTBS match' AS 'Info', nsm.NotificationId, MatchType, UpdateDateTime, ConfidenceLevel, MatchMethod, 
+ CASE WHEN le.Denotified = 'Yes' THEN 'Denotified' ELSE 'Notified' END AS NotificationStatus, rn.NotificationDate, rn.NhsNumber, rn.DateOfBirth, 
+	CONCAT(UPPER(rn.Surname), ', ', rn.Forename) AS PatientName, 
+	rn.Sex, le.AddressLine1, rn.Postcode 
+FROM [NTBS_Specimen_Matching].[dbo].[NotificationSpecimenMatch] nsm
+	INNER JOIN [dbo].[ReusableNotification] rn ON rn.NtbsId = nsm.NotificationID
+	LEFT OUTER JOIN [dbo].[LegacyExtract] le ON le.NtbsId = nsm.NotificationID
+WHERE ReferenceLaboratoryNumber = @ReferenceLaboratoryNumber
+
+--then the ETS record that the NTBS one(s) were migrated from
+
+SELECT 'ETS details of migrated NTBS record' AS 'Info', ets.LegacyId, 
+	CASE 
+		WHEN ets.DenotificationId IS NOT NULL THEN 'Denotified'  
+		WHEN ets.AuditDelete IS NOT NULL THEN 'Deleted'
+		WHEN ets.Submitted = 0 THEN 'Draft'
+		ELSE 'Notified' 
+	END AS NotificationStatus
+	, ets.NotificationDate, p.NhsNumber, p.DateOfBirth, CONCAT(UPPER(p.Surname), ', ', p.Forename) AS PatientName, 
+	p.Sex, CONCAT(a.Line1, a.Line2, a.freetextCityName, a.freetextCountyName) AS PatientAddress, pc.Pcd2
+FROM [NTBS_Specimen_Matching].[dbo].[NotificationSpecimenMatch] nsm
+	INNER JOIN ReusableNotification rn ON rn.NotificationId = nsm.NotificationID
+	INNER JOIN [ets].[dbo].[Notification] ets ON ets.LegacyId = rn.ETSID
+	INNER JOIN [ets].[dbo].[Patient] p ON p.Id = ets.PatientId
+	INNER JOIN [ets].[dbo].[Address] a ON a.Id = ets.AddressId
+	INNER JOIN [ets].[dbo].[Postcode] pc ON pc.Id = a.PostcodeId
+WHERE ReferenceLaboratoryNumber = @ReferenceLaboratoryNumber
+
+
+--ETS matches
+SELECT 'ETS Match' AS Info, esm.LegacyId, EarliestMatchDate, Denotified, Draft, Automatched, 
+	CASE 
+		WHEN ets.DenotificationId IS NOT NULL THEN 'Denotified'  
+		WHEN ets.AuditDelete IS NOT NULL THEN 'Deleted'
+		WHEN ets.Submitted = 0 THEN 'Draft'
+		ELSE 'Notified' 
+	END AS NotificationStatus
+	,ets.NotificationDate, p.NhsNumber, p.DateOfBirth, CONCAT(UPPER(p.Surname), ', ', p.Forename) AS PatientName, 
+	p.Sex, CONCAT(a.Line1, a.Line2, a.freetextCityName, a.freetextCountyName) AS PatientAddress, pc.Pcd2
+FROM [dbo].[EtsSpecimenMatch] esm 
+	INNER JOIN [ets].[dbo].[Notification] ets ON ets.LegacyId = esm.LegacyId
+	INNER JOIN [ets].[dbo].[Patient] p ON p.Id = ets.PatientId
+	INNER JOIN [ets].[dbo].[Address] a ON a.Id = ets.AddressId
+	INNER JOIN [ets].[dbo].[Postcode] pc ON pc.Id = a.PostcodeId
+WHERE ReferenceLaboratoryNumber = @ReferenceLaboratoryNumber
+
+--shot in the dark matches that would not appear in EtsSpecimenMatch - this is where the match has been deleted or the notification has been deleted
+ SELECT n.LegacyId, 
+	CASE WHEN n.AuditDelete IS NOT NULL THEN 'Notification deleted'
+		WHEN lr.AuditDelete IS NOT NULL THEN 'Match deleted' 
+	END AS DeletionReason
+ FROM [ets].[dbo].[LaboratoryResult] lr
+	INNER JOIN [labbase2].[dbo].[Anonymised] a ON a.OpieId = lr.OpieId --AND lr.AuditDelete IS NULL
+	INNER JOIN [ets].[dbo].[Notification] n ON n.Id = lr.NotificationId
+ WHERE ReferenceLaboratoryNumber = @ReferenceLaboratoryNumber
+ AND (n.AuditDelete IS NOT NULL OR lr.AuditDelete IS NOT NULL)
---
+++ source/dbo/Stored Procedures/Lab Results/uspSummarisedNTBSMatches.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dbo].[uspSummarisedNTBSMatches]
+	@StartDate DATE
+AS
+	SELECT  
+		'NTBS' AS 'System', 
+		DATEPART(YEAR, UpdateDateTime) AS 'Year', 
+		SUM(CASE WHEN MatchType = 'Confirmed' THEN 1 ELSE 0 END) AS 'NumberOfConfirmedMatches', 
+		SUM(CASE WHEN MatchMethod = 'Automatch' THEN 1 ELSE 0 END) AS 'NumberOfAutomatches',
+		SUM(CASE WHEN MatchMethod = 'Manual-ConfidenceLevel' THEN 1 ELSE 0 END) AS 'NumberOfConfidenceLevelMatches',
+		SUM(CASE WHEN MatchMethod = 'Manual' THEN 1 ELSE 0 END) AS NumberOfManualMatches,
+		SUM(CASE WHEN MatchType = 'Possible' THEN 1 ELSE 0 END) AS 'NumberOfPotentialMatches',
+		SUM(CASE WHEN MatchType = 'Rejected' THEN 1 ELSE 0 END) AS 'NumberOfRejectedMatches'
+	FROM [$(NTBS_Specimen_Matching)].[dbo].[NotificationSpecimenMatch] 
+	WHERE  MatchMethod != 'Migration'
+		AND UpdateDateTime >= @StartDate
+	GROUP BY DATEPART(YEAR, UpdateDateTime)
+RETURN 0
