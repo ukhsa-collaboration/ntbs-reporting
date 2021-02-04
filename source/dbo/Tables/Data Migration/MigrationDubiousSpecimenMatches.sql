29fdbf3b776a98ee9a028132052f913509806962
Adil Mirza <Adil.Mirza@phe.gov.uk>
2021-02-04 08:30:33 +0000

    NTBS-2067: Initial code - in progress

---
+++ source/dbo/Tables/Data Migration/MigrationDubiousSpecimenMatches.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[MigrationDubiousSpecimenMatches]
+(
+	EtsId [bigint] NOT NULL,
+	ReferenceLaboratoryNumber VARCHAR(50) NOT NULL,
+	SpecimenDateRangeFlag bit null,
+	NHSNumberDifferentFlag bit null, 
+	SpecimenMultipleNotificationMatchFlag bit null,
+	DenotifiedMatchFlag bit null,
+	DeletedDraftFlag bit null
+)
