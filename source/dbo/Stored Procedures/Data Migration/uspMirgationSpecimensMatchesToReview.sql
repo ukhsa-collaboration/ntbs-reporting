aab2aae93f4333e45327bdc77777bdce0eda694c
Adil Mirza <Adil.Mirza@phe.gov.uk>
2021-02-11 10:36:19 +0000

    NTBS-2067: Added uspMirgationSpecimensMatchesToReview for Migration Report

---
+++ source/dbo/Stored Procedures/Data Migration/uspMirgationSpecimensMatchesToReview.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[uspMirgationSpecimensMatchesToReview]
+	@MigrationRun INT = NULL
+AS
+	SELECT 
+		[MigrationNotificationId]			AS 'MigrationNotificationId' 
+		--,[NotificationDate]					AS 'NotificationDate' 
+		--,DATEPART(YEAR, NotificationDate)	AS 'NotificationYear'
+		,[LegacyETSId]						AS 'EtsId'
+		,d.MigrationNotes					AS 'Comments'
+ 
+		FROM [dbo].[MigrationRunResults] mrr
+		INNER JOIN [dbo].[vwMigrationDubiousSpecimenMatches] d on d.EtsId = mrr.LegacyETSId
+		WHERE MigrationRunId = @MigrationRun
