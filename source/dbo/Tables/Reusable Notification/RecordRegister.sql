9b2a0ebbe510f8c9068dfcf0cf35fadcaee21227
Nancy Pickering <nancy@aptemus.com>
2021-04-08 07:57:44 +0100

    NTBS-2217: initial code

---
+++ source/dbo/Tables/Reusable Notification/RecordRegister.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE [dbo].[RecordRegister](
+	-- This primary key
+	[RegisterId] [int] IDENTITY(1,1) NOT NULL,
+	-- Global (ETS, LTBR, NTBS encompassing) primary key
+	[NotificationId] [int] NOT NULL,
+	--Source of record
+	[SourceSystem] [nvarchar](50) NOT NULL,
+	[NotificationDate] [date] NOT NULL,
+	[TBServiceCode] [nvarchar] (50) NULL,
+	[ResidencePhecCode] [nvarchar] (50) NULL,
+	[TreatmentPhecCode] [nvarchar] (50) NULL,
+	[ClusterId] [nvarchar](20) NULL,
+	[Denotified] BIT NULL, 
+    CONSTRAINT [PK_RecordRegister] PRIMARY KEY CLUSTERED (
+		[RegisterId] ASC
+	)
+) ON [PRIMARY]
+GO
+
+CREATE NONCLUSTERED INDEX IX_RecordRegister_NotificationId ON dbo.RecordRegister(NotificationId)
+GO
+CREATE NONCLUSTERED INDEX IX_RecordRegister_NotificationDate ON dbo.RecordRegister(NotificationDate)
+GO
---
+++ source/dbo/Tables/Reusable Notification/Record_PersonalDetails.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[Record_PersonalDetails](
+	-- This primary key
+	[PersonalDetailsId] [int] IDENTITY(1,1) NOT NULL,
+
+	-- Global (ETS, LTBR, NTBS encompassing) primary key
+	[NotificationId] [int] NOT NULL,
+	[NhsNumber] [nvarchar](50) NULL,
+	[Forename] [nvarchar](50) NULL,
+	[Surname] [nvarchar](50) NULL,
+	[DateOfBirth] [date] NULL,
+	[Postcode] [nvarchar](20) null,
+	
+
+	 CONSTRAINT [PK_Record_PersonalDetails] PRIMARY KEY CLUSTERED (
+		[PersonalDetailsId] ASC
+	), 
+    CONSTRAINT [FK_Record_PersonalDetails_ToRecordRegister] FOREIGN KEY ([NotificationId]) REFERENCES [RecordRegister]([NotificationId])
+) ON [PRIMARY]
+GO
+
