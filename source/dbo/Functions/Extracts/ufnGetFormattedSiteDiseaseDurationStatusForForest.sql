903594b2d41173bbf2001904bf095079bce32529
LeoCie <leon.cieslik@softwire.com>
2021-04-27 12:18:44 +0100

    NTBS-2261: Base forest extract on ntbs notification instead of reusable notification

--- source/dbo/Functions/Extracts/ufnGetFormattedSiteDiseaseDurationStatusForForest.sql
+++ source/dbo/Functions/Extracts/ufnGetFormattedSiteDiseaseDurationStatusForForest.sql
@@ -2,12 +2,12 @@
 -- Value for site disease duration status for Forest is either Yes or Empty String (when the status is No, Unknown or Null)
 CREATE FUNCTION dbo.ufnGetFormattedSiteDiseaseDurationStatusForForest
 	(
-		@durationStatus varchar(100)
+		@durationStatus bit
 	)
 	RETURNS varchar(10) 
 	AS
 	BEGIN
 		RETURN
-		CASE @durationStatus WHEN 'Yes' THEN 'Yes' ELSE '' 
+		CASE @durationStatus WHEN 1 THEN 'Yes' ELSE '' 
 	END
 END
