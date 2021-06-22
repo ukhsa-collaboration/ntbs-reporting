b48bb5480e795a0da9d6d4c0ac08b739edefb782
Benji Marshall <Benji.Marshall@softwire.com>
2021-06-22 10:24:01 +0100

    Add TableCounts table and stored proc
    Add a table to keep track of the important migration and specimen
    matching tables and views, counting the number of records in them so
    the app can warn if these tables have become shorter.

---
+++ source/dbo/Stored Procedures/Monitoring/uspUpdateTableCounts.sql
@@ -0,0 +1,77 @@
+﻿CREATE PROCEDURE [dbo].[uspUpdateTableCounts]
+AS
+	INSERT INTO [TableCounts] (CountTime
+		,MigrationNotificationsViewCount
+		,MigrationMBovisAnimalExposureViewCount
+		,MigrationMBovisExposureToKnownCaseViewCount
+		,MigrationMBovisOccupationExposuresViewCount
+		,MigrationMBovisUnpasteurisedMilkConsumptionViewCount
+		,MigrationSocialContextAddressViewCount
+		,MigrationSocialContextVenueViewCount
+		,MigrationTransferEventsViewCount
+		,MigrationTreatmentOutcomeEventsViewCount
+		,EtsNotificationsCount
+		,LtbrNotificationsCount
+		,ETS_NotificationCount
+		,LTBR_DiseasePeriodCount
+		,LTBR_PatientEpisodeCount
+		,NotificationClusterMatchCount
+		,NotificationSpecimenMatchCount
+		,EtsSpecimenMatchCount
+	)
+
+	SELECT
+		GETDATE() AS CountTime
+		,(SELECT COUNT(*)
+			FROM [$(migration)].dbo.MigrationNotificationsView
+		) AS MigrationNotificationsViewCount
+		,(SELECT COUNT(*)
+			FROM [$(migration)].dbo.MigrationMBovisAnimalExposureView
+		) AS MigrationMBovisAnimalExposureViewCount
+		,(SELECT COUNT(*)
+			FROM [$(migration)].dbo.MigrationMBovisExposureToKnownCaseView
+		) AS MigrationMBovisExposureToKnownCaseViewCount
+		,(SELECT COUNT(*)
+			FROM [$(migration)].dbo.MigrationMBovisOccupationExposuresView
+		) AS MigrationMBovisOccupationExposuresViewCount
+		,(SELECT COUNT(*)
+			FROM [$(migration)].dbo.MigrationMBovisUnpasteurisedMilkConsumptionView
+		) AS MigrationMBovisUnpasteurisedMilkConsumptionViewCount
+		,(SELECT COUNT(*)
+			FROM [$(migration)].dbo.MigrationSocialContextAddressView
+		) AS MigrationSocialContextAddressViewCount
+		,(SELECT COUNT(*)
+			FROM [$(migration)].dbo.MigrationSocialContextVenueView
+		) AS MigrationSocialContextVenueViewCount
+		,(SELECT COUNT(*)
+			FROM [$(migration)].dbo.MigrationTransferEventsView
+		) AS MigrationTransferEventsViewCount
+		,(SELECT COUNT(*)
+			FROM [$(migration)].dbo.MigrationTreatmentOutcomeEventsView
+		) AS MigrationTreatmentOutcomeEventsViewCount
+		,(SELECT COUNT(*)
+			FROM [$(migration)].dbo.EtsNotifications
+		) AS EtsNotificationsCount
+		,(SELECT COUNT(*)
+			FROM [$(migration)].dbo.LtbrNotifications
+		) AS LtbrNotificationsCount
+		,(SELECT COUNT(*)
+			FROM [$(migration)].dbo.ETS_Notification
+		) AS ETS_NotificationCount
+		,(SELECT COUNT(*)
+			FROM [$(migration)].dbo.LTBR_DiseasePeriod
+		) AS LTBR_DiseasePeriodCount
+		,(SELECT COUNT(*)
+			FROM [$(migration)].dbo.LTBR_PatientEpisode
+		) AS LTBR_PatientEpisodeCount
+		,(SELECT COUNT(*)
+			FROM [$(NTBS_Specimen_Matching)].dbo.NotificationClusterMatch
+		) AS NotificationClusterMatchCount
+		,(SELECT COUNT(*)
+			FROM [$(NTBS_Specimen_Matching)].dbo.NotificationSpecimenMatch
+		) AS NotificationSpecimenMatchCount
+		,(SELECT COUNT(*)
+			FROM [$(NTBS_Specimen_Matching)].dbo.EtsSpecimenMatch
+		) AS EtsSpecimenMatchCount
+
+RETURN 0
---
+++ source/dbo/Tables/Monitoring/TableCounts.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE [dbo].[TableCounts]
+(
+	[CountTime] DATETIME2 NOT NULL PRIMARY KEY,
+	[MigrationNotificationsViewCount] INT NOT NULL,
+	[MigrationMBovisAnimalExposureViewCount] INT NOT NULL,
+	[MigrationMBovisExposureToKnownCaseViewCount] INT NOT NULL,
+	[MigrationMBovisOccupationExposuresViewCount] INT NOT NULL,
+	[MigrationMBovisUnpasteurisedMilkConsumptionViewCount] INT NOT NULL,
+	[MigrationSocialContextAddressViewCount] INT NOT NULL,
+	[MigrationSocialContextVenueViewCount] INT NOT NULL,
+	[MigrationTransferEventsViewCount] INT NOT NULL,
+	[MigrationTreatmentOutcomeEventsViewCount] INT NOT NULL,
+	[EtsNotificationsCount] INT NOT NULL,
+	[LtbrNotificationsCount] INT NOT NULL,
+	[ETS_NotificationCount] INT NOT NULL,
+	[LTBR_DiseasePeriodCount] INT NOT NULL,
+	[LTBR_PatientEpisodeCount] INT NOT NULL,
+	[NotificationClusterMatchCount] INT NOT NULL,
+	[NotificationSpecimenMatchCount] INT NOT NULL,
+	[EtsSpecimenMatchCount] INT NOT NULL
+)
