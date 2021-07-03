d6dc7474a60860a9ba0a409256630673b711f364
Nancy <nancy@aptemus.com>
2021-07-03 21:15:17 +0100

    Initial views for a monitoring dashboard

---
+++ source/dbo/Views/Power BI Reporting/vwAlert.sql
@@ -0,0 +1,16 @@
+﻿CREATE VIEW [dbo].[vwAlert]
+	AS
+	SELECT 
+		a.NotificationId,
+		a.AlertId,
+		a.AlertStatus,
+		a.AlertType,
+		a.ClosingUserId,
+		a.ClosureDate,
+		a.CreationDate,
+		a.DuplicateId,
+		a.SpecimenId,
+		hd.TBServiceCode
+
+  FROM [$(NTBS)].[dbo].Alert a
+		LEFT OUTER JOIN [$(NTBS)].[dbo].[HospitalDetails] hd ON hd.NotificationId = a.NotificationId
---
+++ source/dbo/Views/Power BI Reporting/vwHangfireJobs.sql
@@ -0,0 +1,12 @@
+﻿CREATE VIEW [dbo].[vwHangfireJobs]
+	AS
+	SELECT 
+		SUBSTRING(Q1.InvocationData, 25, EndOfString-25) AS JobName, 
+		Q1.StateName, 
+		Q1.CreatedAt 
+	FROM
+		(SELECT StateName, CreatedAt, CHARINDEX(',',InvocationData, 1) AS EndOfString, InvocationData
+		FROM [$(NTBS)].[HangFire].[Job]) AS Q1
+
+  --we want the text after {"t":"ntbs_service.Jobs.UserSyncJob, ntbs-service","m":"Run","p":["Hangfire.IJobCancellationToken, Hangfire.Core"]}
+  --and before the first comma
