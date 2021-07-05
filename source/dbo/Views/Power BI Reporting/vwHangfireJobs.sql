c858ad581be6d0b90b0a9edd66540ffa79439718
Nancy Pickering <nancy@aptemus.com>
2021-07-05 18:54:32 +0100

    Update to where clause to exclude data migration jobs

--- source/dbo/Views/Power BI Reporting/vwHangfireJobs.sql
+++ source/dbo/Views/Power BI Reporting/vwHangfireJobs.sql
@@ -6,7 +6,9 @@
 		Q1.CreatedAt 
 	FROM
 		(SELECT StateName, CreatedAt, CHARINDEX(',',InvocationData, 1) AS EndOfString, InvocationData
-		FROM [$(NTBS)].[HangFire].[Job]) AS Q1
+		FROM [$(NTBS)].[HangFire].[Job]
+		WHERE InvocationData LIKE '%ntbs_service.Jobs.%'
+		) AS Q1
 
   --we want the text after {"t":"ntbs_service.Jobs.UserSyncJob, ntbs-service","m":"Run","p":["Hangfire.IJobCancellationToken, Hangfire.Core"]}
   --and before the first comma
