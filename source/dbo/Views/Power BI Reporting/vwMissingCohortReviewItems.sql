17b372fc994aac94510edcbf3224cae8caa21158
LeoCie <leon.cieslik@softwire.com>
2021-12-13 10:04:08 +0000

    NTBS-2765: Add treatment end date check and fix other fields

--- source/dbo/Views/Power BI Reporting/vwMissingCohortReviewItems.sql
+++ source/dbo/Views/Power BI Reporting/vwMissingCohortReviewItems.sql
@@ -8,10 +8,11 @@
 	CASE WHEN cd.EthnicGroup = 'Not stated' THEN 'Yes' END AS EthnicGroup,
 	CASE WHEN cd.BirthCountry = 'Unknown' THEN 'Yes' END AS CountryOfBirth,
 	CASE WHEN cd.BirthCountry <> 'United Kingdom' AND cd.UkEntryYear IS NULL THEN 'Yes' END AS UkEntry,
-	CASE WHEN cd.SputumResult = 'No result' THEN 'Yes' END AS SputumResult,
+	CASE WHEN cd.SputumResult = 'Unknown' THEN 'Yes' END AS SputumResult,
 	CASE WHEN car.CulturePositive = 'No' AND cd.CultureSummary = 'No result' THEN 'Yes' END AS CultureResult,
 	CASE WHEN car.CulturePositive = 'Yes' AND car.DrugResistanceProfile = 'No result' THEN 'Yes' END AS AntibioticSensitivity,
 	CASE WHEN cd.FirstPresentationDate IS NULL AND cd.FirstPresentationSetting IS NULL THEN 'Yes' END AS FirstPresentation,
+	CASE WHEN cd.TreatmentEndDate IS NULL THEN 'Yes' END AS TreatmentEndDate,
 	CASE WHEN cd.EnhancedCaseManagement IS NULL
 		OR cd.EnhancedCaseManagement = 'Unknown'
 		OR (cd.EnhancedCaseManagement = 'Yes' AND cd.EnhancedCaseManagementLevel IS NULL) THEN 'Yes' END AS ECM,
@@ -19,7 +20,7 @@
 	CASE WHEN cd.DOTOffered IS NULL
 		OR cd.DOTOffered = 'Unknown'
 		OR (cd.DOTOffered = 'Yes' AND cd.DOTReceived IS NULL) THEN 'Yes' END AS DOT,
-	CASE WHEN cd.LastRecordedTreatmentOutcome IS NULL THEN 'Yes' END AS OutcomeRecorded,
+	CASE WHEN cd.LastRecordedTreatmentOutcome = 'No outcome recorded' THEN 'Yes' END AS OutcomeRecorded,
 	CASE WHEN cd.ChildContactsIdentified IS NULL AND cd.AdultContactsIdentified IS NULL THEN 'Yes' END AS ContactsIdentified,
 	CASE WHEN cd.ChildContactsAssessed IS NULL AND cd.AdultContactsAssessed IS NULL THEN 'Yes' END AS ContactsScreened,
 	CASE WHEN cd.ChildContactsLTBI IS NULL AND cd.AdultContactsLTBI IS NULL THEN 'Yes' END AS ContactsLTBI,
@@ -34,7 +35,7 @@
 	WHERE NOT (
 		Sex IS NULL AND EthnicGroup IS NULL AND CountryOfBirth IS NULL AND
 		SputumResult IS NULL AND CultureResult IS NULL AND AntibioticSensitivity IS NULL AND
-		FirstPresentation IS NULL AND ECM IS NULL AND HIV IS NULL AND DOT IS NULL AND
+		FirstPresentation IS NULL AND TreatmentEndDate IS NULL AND ECM IS NULL AND HIV IS NULL AND DOT IS NULL AND
 		OutcomeRecorded IS NULL AND ContactsIdentified IS NULL AND ContactsScreened IS NULL AND
 		ContactsLTBI IS NULL AND ContactsLTBIStartedTreatment IS NULL AND ContactsLTBICompletedTreatment IS NULL
 	)
