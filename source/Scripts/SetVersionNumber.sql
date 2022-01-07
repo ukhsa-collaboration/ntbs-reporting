5458a71d1a4380db37742dfe583e9e7807ae6ec9
Joshua Cadney <joshua.cadney@softwire.com>
2022-01-07 09:55:47 +0000

    Bump version number

--- source/Scripts/SetVersionNumber.sql
+++ source/Scripts/SetVersionNumber.sql
@@ -1,4 +1,4 @@
 ﻿TRUNCATE TABLE [dbo].[ReleaseVersion]
 
 INSERT INTO [dbo].[ReleaseVersion]([Version],[Date])
-    VALUES ('1.11.0', '2021-12-16 12:00:00')
+    VALUES ('1.11.2', '2022-01-07 12:00:00')
