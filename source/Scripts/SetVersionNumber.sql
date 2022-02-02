0dd74bd86d2c0eb932ef4433782b798642c51299
Joshua Cadney <joshua.cadney@softwire.com>
2022-02-02 16:54:15 +0000

    Update release date

--- source/Scripts/SetVersionNumber.sql
+++ source/Scripts/SetVersionNumber.sql
@@ -1,4 +1,4 @@
 ﻿TRUNCATE TABLE [dbo].[ReleaseVersion]
 
 INSERT INTO [dbo].[ReleaseVersion]([Version],[Date])
-    VALUES ('1.12.1', '2022-02-02 12:00:00')
+    VALUES ('1.12.1', '2022-02-03 12:00:00')
