b8d94d58a6fb583ee2f2fd27f41f6cc2c5071826
Jordan <jordan.d.carlton@gmail.com>
2022-03-09 09:21:16 +0000

    NTBS-3052 add unmatched specimen and unmatched specimen linked notification

---
+++ source/dbo/Views/Lab Results/vwUnmatchedSpecimens.sql
@@ -0,0 +1,79 @@
+﻿CREATE VIEW [dbo].[vwUnmatchedSpecimens]
+	AS 
+
+	WITH NoMatchesFound AS
+	(
+		SELECT ls.ReferenceLaboratoryNumber
+		FROM [$(NTBS_Specimen_Matching)].dbo.LabSpecimen ls
+		INNER JOIN vwNotificationYear ny ON ny.NotificationYear = YEAR(ls.SpecimenDate)
+		LEFT JOIN [$(NTBS_Specimen_Matching)].dbo.NotificationSpecimenMatch nsm ON nsm.ReferenceLaboratoryNumber = ls.ReferenceLaboratoryNumber
+		WHERE nsm.MatchType IS NULL
+	),
+
+	MatchTypeRanking AS
+	(
+
+		SELECT 1 AS [Rank], 'Confirmed' AS ResultName
+		UNION
+		SELECT 1 AS [Rank], 'Possible' AS ResultName
+		UNION
+		SELECT 2 AS [Rank], 'Rejected-Possible' AS ResultName
+		UNION
+		SELECT 2 AS [Rank], 'Rejected' AS ResultName
+	),
+
+	AllMatchesRejected AS
+	(
+		SELECT ranked.ReferenceLaboratoryNumber FROM (
+			SELECT DISTINCT nsm.ReferenceLaboratoryNumber, FIRST_VALUE(mtr.ResultName) OVER (PARTITION BY nsm.ReferenceLaboratoryNumber ORDER BY mtr.[rank] ASC) AS [Result]
+			FROM [$(NTBS_Specimen_Matching)].dbo.NotificationSpecimenMatch nsm
+			INNER JOIN [$(NTBS_Specimen_Matching)].dbo.LabSpecimen ls on ls.ReferenceLaboratoryNumber = nsm.ReferenceLaboratoryNumber
+			INNER JOIN vwNotificationYear ny ON ny.NotificationYear = YEAR(ls.SpecimenDate)
+			INNER JOIN MatchTypeRanking mtr ON mtr.ResultName = nsm.MatchType) ranked
+		WHERE ranked.Result = 'Rejected' OR ranked.Result = 'Rejected-Possible'
+	),
+
+	UnmatchedSpecimens AS
+	(
+		SELECT * FROM NoMatchesFound 
+		UNION 
+		SELECT * FROM AllMatchesRejected
+	)
+
+	SELECT DISTINCT ls.[ReferenceLaboratoryNumber]
+			,ls.[SpecimenDate]
+			,ls.[SpecimenTypeCode] AS SampleType
+			,ls.[LaboratoryName]
+			,ls.[ReferenceLaboratory]
+			,ls.[Species]
+			,CASE WHEN ls.[INH] = 'Sensitive' THEN 'S'
+				WHEN ls.[INH] = 'Resistant' THEN 'R'
+				WHEN ls.[INH] = 'No result' THEN 'N' ELSE NULL END AS [INH]
+			,CASE WHEN ls.RIF = 'Sensitive' THEN 'S'
+				WHEN ls.RIF = 'Resistant' THEN 'R'
+				WHEN ls.RIF = 'No result' THEN 'N' ELSE NULL END AS [RIF]
+			,CASE WHEN ls.PZA = 'Sensitive' THEN 'S'
+				WHEN ls.PZA = 'Resistant' THEN 'R'
+				WHEN ls.PZA = 'No result' THEN 'N' ELSE NULL END AS [PZA]
+			,CASE WHEN ls.EMB = 'Sensitive' THEN 'S'
+				WHEN ls.EMB = 'Resistant' THEN 'R'
+				WHEN ls.EMB = 'No result' THEN 'N' ELSE NULL END AS [EMB]
+			,ls.[MDR]
+			,ls.[AMINO]
+			,ls.[QUIN]
+			,ls.[XDR]
+			,ls.[PatientNhsNumber]
+			,ls.[PatientBirthDate]
+			,ls.[PatientName]
+			,ls.[PatientSex]
+			,ls.[PatientAddress]
+			,ls.[PatientPostcode]
+			-- Take first value over partition as each lab name can have multiple mappings, 
+			-- some of which lead to null regions, others which don't
+			,FIRST_VALUE(p.[Name]) OVER (PARTITION BY us.ReferenceLaboratoryNumber ORDER BY p.[Name] DESC) AS Region
+		FROM UnmatchedSpecimens us
+		JOIN [$(NTBS_Specimen_Matching)].dbo.LabSpecimen ls on ls.ReferenceLaboratoryNumber = us.ReferenceLaboratoryNumber
+		LEFT JOIN [$(ETS)].dbo.LaboratoryHospitalMapping lhm on lhm.SourceLabName = ls.LaboratoryName AND lhm.AuditDelete is NULL
+		LEFT JOIN [$(NTBS)].ReferenceData.Hospital hd on lhm.HospitalId = hd.HospitalId
+		LEFT JOIN [$(NTBS)].ReferenceData.TbService tb on hd.TBServiceCode = tb.Code
+		LEFT JOIN [$(NTBS)].ReferenceData.PHEC p on p.Code = tb.PHECCode
---
+++ source/dbo/Views/Lab Results/vwUnmatchedSpecimensLinkedNotifications.sql
@@ -0,0 +1,50 @@
+﻿CREATE VIEW [dbo].[vwUnmatchedSpecimensLinkedNotifications]
+	AS 
+	
+	SELECT us.ReferenceLaboratoryNumber
+		,CASE
+			WHEN nsm.MatchType = 'Rejected-Possible' THEN 'Possible match rejected by ' + u.DisplayName + ' on ' + FORMAT(a.AuditDateTime, 'dd MMM yyyy')
+			WHEN nsm.MatchType = 'Rejected' THEN 'Confirmed match rejected by ' + u.DisplayName + ' on ' + FORMAT(a.AuditDateTime, 'dd MMM yyyy')
+		END AS Notes
+		,nsm.NotificationID
+		,p.NhsNumber
+		,p.Dob
+		from vwUnmatchedSpecimens us
+	JOIN [$(NTBS_Specimen_Matching)].dbo.NotificationSpecimenMatch nsm on nsm.ReferenceLaboratoryNumber = us.ReferenceLaboratoryNumber
+	JOIN [$(NTBS_AUDIT)].dbo.AuditLogs a on a.OriginalId = nsm.ReferenceLaboratoryNumber
+	JOIN [$(NTBS)].dbo.[User] u on u.Username = a.AuditUser
+	JOIN [$(NTBS)].dbo.Patients p on p.NotificationId = nsm.NotificationId
+	WHERE nsm.MatchType = 'Rejected' or nsm.MatchType = 'Rejected-Possible'
+
+	--,
+
+	--Poor quality matches?
+	--Unmatched by notification?
+	--If any confirmed, don't need any rows?
+
+	--Start at vwUnmatchedSpecimens, join to nsm, join to audit logs
+	--nsm has an edit field? Where to get history?
+	--When matched/unmatched, nsm is just updated.
+	--Could look at alerts?
+	--Could look at audit logs again
+	--Only 400 unmatched specimens
+
+	--AllMatches AS
+	--(
+	--	SELECT * FROM PreviousPossibleMatches 
+	--	UNION 
+	--	SELECT * FROM PreviousConfirmedMatches
+	--)
+	
+	
+	--SELECT ReferenceLaboratoryNumber
+	--		,CASE
+	--			WHEN am.MatchType = 'Rejected-Possible' THEN 'Possible match rejected by ' + am.DisplayName + ' on ' + FORMAT(am.AuditDateTime, 'dd MMM yyyy')
+	--			WHEN am.MatchType = 'Rejected' THEN 'Confirmed match rejected by ' + am.DisplayName + ' on ' + FORMAT(am.AuditDateTime, 'dd MMM yyyy')
+	--		END AS Notes
+	--		,am.NotificationID
+	--		,p.NhsNumber
+	--		,p.Dob
+	--FROM AllMatches am
+	--JOIN [$(NTBS)].dbo.Patients p on p.NotificationId = am.NotificationId
+
