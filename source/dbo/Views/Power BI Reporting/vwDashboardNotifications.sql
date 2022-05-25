cc432bd1836be634d27748a2a52be61ae0bf9f86
Nancy Pickering <nancy@aptemus.com>
2022-05-25 11:44:36 +0100

    NTBS-3125 - further update to view to include cases with an outcome but which still have open alerts

--- source/dbo/Views/Power BI Reporting/vwDashboardNotifications.sql
+++ source/dbo/Views/Power BI Reporting/vwDashboardNotifications.sql
@@ -1,5 +1,5 @@
 ﻿--this view supports the 'NTBS dashboard' report which shows users their cases which do not yet have a final treatment outcome 
---applied to them. It's also used to display which notifications have which open alerts, and so may include cases
+--applied to them, as well as notifications with open alerts, This may include cases
 --not yet in their service/region but where this is an open transfer request
 
 CREATE VIEW [dbo].[vwDashboardNotifications]
@@ -14,10 +14,14 @@ CREATE VIEW [dbo].[vwDashboardNotifications]
 		END AS 'OutcomeStatus'
 		,openTransfer.AlertTBServiceCode
 		,tbsp.PHEC_Code AS AlertPhecCode
-	FROM [dbo].[Record_CaseData] cd
-	INNER JOIN [dbo].[RecordRegister] rr ON rr.NotificationId = cd.NotificationId AND rr.Denotified = 0
-	INNER JOIN [dbo].[Outcome] o ON o.NotificationId = cd.NotificationId
-	LEFT OUTER JOIN [dbo].[vwAlert] overdueOutcome ON overdueOutcome.NotificationId = cd.NotificationId AND overdueOutcome.AlertType LIKE '%DataQualityTreatmentOutcome%'
-	LEFT OUTER JOIN [dbo].[vwAlert] openTransfer ON openTransfer.NotificationId = cd.NotificationId AND openTransfer.AlertType LIKE '%TransferRequest%' AND openTransfer.AlertStatus = 'Open'
-	LEFT OUTER JOIN [$(NTBS_R1_Geography_Staging)].[dbo].[TB_Service_to_PHEC] tbsp ON tbsp.TB_Service_Code = openTransfer.AlertTBServiceCode
-WHERE cd.LastRecordedTreatmentOutcomeDescriptive IN ('No outcome recorded', 'Not evaluated - still on treatment') AND rr.Denotified = 0
+		FROM [dbo].[Record_CaseData] cd
+		INNER JOIN [dbo].[RecordRegister] rr ON rr.NotificationId = cd.NotificationId AND rr.Denotified = 0
+		INNER JOIN [dbo].[Outcome] o ON o.NotificationId = cd.NotificationId
+		LEFT OUTER JOIN [dbo].[vwAlert] overdueOutcome ON overdueOutcome.NotificationId = cd.NotificationId AND overdueOutcome.AlertType LIKE '%DataQualityTreatmentOutcome%'
+		LEFT OUTER JOIN [dbo].[vwAlert] openTransfer ON openTransfer.NotificationId = cd.NotificationId AND openTransfer.AlertType LIKE '%TransferRequest%' AND openTransfer.AlertStatus = 'Open'
+		LEFT OUTER JOIN [dbo].[vwAlert] anyAlert ON anyAlert.NotificationId = cd.NotificationId AND anyAlert.AlertStatus = 'Open'
+		LEFT OUTER JOIN [$(NTBS_R1_Geography_Staging)].[dbo].[TB_Service_to_PHEC] tbsp ON tbsp.TB_Service_Code = openTransfer.AlertTBServiceCode
+	WHERE rr.Denotified = 0
+	--case has no outcome or case has an open alert
+	AND (cd.LastRecordedTreatmentOutcomeDescriptive IN ('No outcome recorded', 'Not evaluated - still on treatment') 
+	OR anyAlert.NotificationId IS NOT NULL)
