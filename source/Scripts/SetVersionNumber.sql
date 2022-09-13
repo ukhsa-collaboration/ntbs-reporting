a0bff6990523029a204a82b8f5363b977adffe26
adilmirzaphe <54138029+adilmirzaphe@users.noreply.github.com>
2022-09-13 08:06:59 +0100

    Update SetVersionNumber.sql

--- source/Scripts/SetVersionNumber.sql
+++ source/Scripts/SetVersionNumber.sql
@@ -1,4 +1,4 @@
 ﻿TRUNCATE TABLE [dbo].[ReleaseVersion]
 
 INSERT INTO [dbo].[ReleaseVersion]([Version],[Date])
-    VALUES ('1.17.0', '2022-06-29 12:00:00')
+    VALUES ('1.17.1', '2022-09-15 12:00:00')
