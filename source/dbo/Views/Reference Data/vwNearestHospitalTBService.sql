7057dfe5ae0b177073ba166a7bf1517829e43221
India Clancy <India.Clancy@ukhsa.gov.uk>
2022-10-07 11:12:43 +0100

    View added for NearestHospitalTBService

---
+++ source/dbo/Views/Reference Data/vwNearestHospitalTBService.sql
@@ -0,0 +1,16 @@
+﻿CREATE VIEW [dbo].[vwNearestHospitalTBService]
+	AS 
+
+	SELECT
+	Postcode,
+	h.HospitalName,
+	TB_Service_Name
+	FROM [$(NTBS_R1_Geography_Staging)].[dbo].[Reduced_Postcode_file] p
+	INNER JOIN [$(NTBS_R1_Geography_Staging)].[dbo].[Hospital] h ON
+	h.HospitalId = p.Closest_Hospital_ID 
+	INNER JOIN [$(NTBS_R1_Geography_Staging)].[dbo].[TB_Service_to_Hospital] t ON
+	t.HospitalID = p.Closest_Hospital_ID
+	INNER JOIN [$(NTBS_R1_Geography_Staging)].[dbo].[TB_Service] tb ON
+	tb.TB_Service_Code = t.TB_Service_Code
+
+
