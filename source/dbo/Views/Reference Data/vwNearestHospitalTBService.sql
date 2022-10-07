2890de9e0c6d6ae1ee793b9b98afa4f9c9dad6b5
India Clancy <India.Clancy@ukhsa.gov.uk>
2022-10-07 11:30:31 +0100

    Updated script for nearest hospital TB Service view.

--- source/dbo/Views/Reference Data/vwNearestHospitalTBService.sql
+++ source/dbo/Views/Reference Data/vwNearestHospitalTBService.sql
@@ -2,14 +2,14 @@
 	AS 
 
 	SELECT
-	Postcode,
-	h.HospitalName,
-	TB_Service_Name
+	p.Pcode as 'Postcode',
+	h.HospitalName as 'Nearest_Hospital',
+	TB_Service_Name as 'Nearest_TBService'
 	FROM [$(NTBS_R1_Geography_Staging)].[dbo].[Reduced_Postcode_file] p
 	INNER JOIN [$(NTBS_R1_Geography_Staging)].[dbo].[Hospital] h ON
 	h.HospitalId = p.Closest_Hospital_ID 
 	INNER JOIN [$(NTBS_R1_Geography_Staging)].[dbo].[TB_Service_to_Hospital] t ON
-	t.HospitalID = p.Closest_Hospital_ID
+	t.HospitalID = h.HospitalId
 	INNER JOIN [$(NTBS_R1_Geography_Staging)].[dbo].[TB_Service] tb ON
 	tb.TB_Service_Code = t.TB_Service_Code
 
