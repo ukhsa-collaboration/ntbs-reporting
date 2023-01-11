5815e7ac94690a0a9025c44797b873b24b48b5bf
adilmirzaphe <54138029+adilmirzaphe@users.noreply.github.com>
2023-01-11 11:52:22 +0000

    Update SetVersionNumber.sql

--- source/Scripts/SetVersionNumber.sql
+++ source/Scripts/SetVersionNumber.sql
@@ -1,4 +1,4 @@
 ﻿TRUNCATE TABLE [dbo].[ReleaseVersion]
 
 INSERT INTO [dbo].[ReleaseVersion]([Version],[Date])
-    VALUES ('1.18.0', '2022-11-23 12:00:00')
+    VALUES ('1.18.1', '2023-01-12 12:00:00')
