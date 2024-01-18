f41f2379a648a2d9612946993034b26afac0ae2b
adilmirzaphe <54138029+adilmirzaphe@users.noreply.github.com>
2024-01-18 14:46:37 +0000

    include all years from 2018 in production

--- source/dbo/Views/Reusable Drop Downs/vwNotificationYear.sql
+++ source/dbo/Views/Reusable Drop Downs/vwNotificationYear.sql
@@ -1,42 +1,21 @@
 ﻿/***************************************************************************************************
-Desc:    This SQL query goes 3 calendar years back, eg when in 2019 the following would only include
-         notifications starting from '2016-01-01'. This DB view is used for the following 2 purposes:
-		 a) This restricts the notification generation query that runs every night to the lst 3 years.
-		 b) This also returns drop-down values of the 12 years, which are used for the end-user to filter
-         on the notification start/end months.
-
-
+Desc:    This SQL query is used to:
+		 a) This restricts the notification generation query that runs every night to the lst n years (where n is a deployment variable
+		    applied in the publish scripts.
          
 **************************************************************************************************/
 
 CREATE VIEW [dbo].[vwNotificationYear] AS
-	SELECT TOP 6 
-		Id,
-		NotificationYear
-	FROM (
-			SELECT
-				0 AS Id,
-				YEAR(GETDATE()) AS NotificationYear
-			UNION
-			SELECT
-				-1 AS Id,
-				YEAR(DATEADD(YEAR, DATEDIFF(YEAR, 0, GETDATE()) -1, 0)) AS NotificationYear
-			UNION
-			SELECT
-				-2 AS Id,
-				YEAR(DATEADD(YEAR, DATEDIFF(YEAR, 0, GETDATE()) -2, 0)) AS NotificationYear
-			UNION
-			SELECT
-				-3 AS Id,
-				YEAR(DATEADD(YEAR, DATEDIFF(YEAR, 0, GETDATE()) -3, 0)) AS NotificationYear
-			UNION
-			SELECT
-				-4 AS Id,
-				YEAR(DATEADD(YEAR, DATEDIFF(YEAR, 0, GETDATE()) -4, 0)) AS NotificationYear
-						UNION
-			SELECT
-				-5 AS Id,
-				YEAR(DATEADD(YEAR, DATEDIFF(YEAR, 0, GETDATE()) -5, 0)) AS NotificationYear
-		) NotificationYear
-	WHERE Id > CONVERT(INT, '-$(NumberOfReportingYears)')
-	ORDER BY NotificationYear DESC
+
+WITH NotificationYears AS 
+(
+    SELECT 0 AS Id,YEAR(GetDate()) AS NotificationYear
+    UNION ALL
+    SELECT ny.Id - 1 as Id,ny.NotificationYear -1  as NotificationYear
+    FROM NotificationYears ny
+    WHERE ny.NotificationYear - 1 >= CONVERT(INT, '$(ReportingStartYear)')
+)
+
+SELECT TOP 100 Id,NotificationYear FROM NotificationYears 
+ORDER BY NotificationYear DESC
+
